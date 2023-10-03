8e674edb65dc2cc17ab498337fa1d3a9a72311d0
Daniel <dnl.canedo@gmail.com>
2023-10-02 23:40:11 -0400

    make RecentTracksParams properties vars

--- Sources/swiftfm/Models/Params/RecentTracksParams.swift
+++ Sources/swiftfm/Models/Params/RecentTracksParams.swift
@@ -1,11 +1,11 @@
 import Foundation
 
 public struct RecentTracksParams {
-    let user: String
-    let limit: UInt
-    let page: UInt
-    let from: UInt?
-    let to: UInt?
+    public var user: String
+    public var limit: UInt
+    public var page: UInt
+    public var from: UInt?
+    public var to: UInt?
 
     public init(user: String, limit: UInt = 50, page: UInt = 1, from: UInt? = nil, to: UInt? = nil) {
         self.user = user
