2a082c807cc88e253b9bf26670ed48a66bcc405e
Daniel <dnl.canedo@gmail.com>
2023-10-08 10:26:28 -0400

    LF-59 Add tests for RecentTrack model

--- Tests/swiftfmTests/swift_fmTests.swift
+++ Tests/swiftfmTests/swift_fmTests.swift
@@ -1,11 +1,11 @@
 import XCTest
-@testable import swift_fm
+@testable import swiftfm
 
 final class swift_fmTests: XCTestCase {
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(swift_fm().text, "Hello, World!")
+//        XCTAssertEqual(SwiftFM().text, "Hello, World!")
     }
 }
