f0060be5ca248011b446ba941c9cc8bc713e5bbf
Daniel <dnl.canedo@gmail.com>
2023-10-12 21:24:44 -0400

    LF-12 Add functionality for retrieving User->getWeeklyTrackChart

---
+++ Sources/swiftfm/Models/CollectionList.swift
@@ -0,0 +1,36 @@
+import Foundation
+
+public struct CollectionList<T: Decodable>: Decodable {
+    struct CodingKeys: CodingKey {
+        var stringValue: String
+
+        init?(stringValue: String) {
+            self.stringValue = stringValue
+        }
+
+        var intValue: Int?
+
+        init?(intValue: Int) {
+            return nil
+        }
+    }
+
+    enum InnerCodingKeys: String, CodingKey {
+        case items = "track"
+    }
+
+    public let items: [T]
+
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+
+        guard let rootKey = container.allKeys.first else {
+            throw RuntimeError("Error at getting root key.")
+        }
+
+        let subcontainer = try container.nestedContainer(keyedBy: InnerCodingKeys.self, forKey: rootKey)
+
+        self.items = try subcontainer.decode([T].self, forKey: .items)
+    }
+}
+
