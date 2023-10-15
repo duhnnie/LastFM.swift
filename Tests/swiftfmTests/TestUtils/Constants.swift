11b00c9a7a5e7873dad5aab76720a3fe57974a25
Daniel <dnl.canedo@gmail.com>
2023-10-15 14:23:50 -0400

    LF-77 Add tests for invalid parameters response

--- Tests/swiftfmTests/TestUtils/Constants.swift
+++ Tests/swiftfmTests/TestUtils/Constants.swift
@@ -6,6 +6,7 @@ internal struct Constants {
     internal static let API_SECRET = "someAPISecret"
 
     internal static let RESPONSE_200_OK = Util.createResponse()
+    internal static let RESPONSE_400_BAD_REQUEST = Util.createResponse(statusCode: 400)
     internal static let RESPONSE_403_FORBIDDEN = Util.createResponse(statusCode: 403)
 
 }
