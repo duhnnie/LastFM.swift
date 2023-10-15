87aa5e1a4564ae399a016663103e0dbd48bbd377
Daniel <dnl.canedo@gmail.com>
2023-10-15 19:34:13 -0400

    LF-37 Add functionality char->getTopTracks

---
+++ Sources/LastFM/Models/Params/ChartTopTrackParams.swift
@@ -0,0 +1,20 @@
+import Foundation
+
+public struct ChartTopTracksParams: Params {
+
+    public var page: UInt
+    public var limit: UInt
+
+    public init(page: UInt = 1, limit: UInt = 50) {
+        self.page = page
+        self.limit = limit
+    }
+
+    func toDictionary() -> Dictionary<String, String> {
+        return [
+            "page": String(page),
+            "limit": String(limit)
+        ]
+    }
+    
+}
