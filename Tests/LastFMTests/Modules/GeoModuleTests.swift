5c658056f6b87794e125d27c8b5d969a97b7bc71
Daniel <dnl.canedo@gmail.com>
2023-10-23 19:53:52 -0400

    LF-33 add unit tests for geo->getTopArtists

--- Tests/LastFMTests/Modules/GeoModuleTests.swift
+++ Tests/LastFMTests/Modules/GeoModuleTests.swift
@@ -106,4 +106,66 @@ class GeoModuleTests: XCTestCase {
 
         waitForExpectations(timeout: 3)
     }
+
+    func test_getTopArtists_success() throws {
+        let jsonURL = Bundle.module.url(
+            forResource: "Resources/geo.getTopArtists",
+            withExtension: "json"
+        )!
+
+        let fakeData = try Data(contentsOf: jsonURL)
+        let expectation = expectation(description: "Waiting for getTopArtists")
+        let params = GeoTopArtistsParams(country: "australia", limit: 5, page: 1)
+
+        apiClient.data = fakeData
+        apiClient.response = Constants.RESPONSE_200_OK
+
+        instance.getTopArtists(params: params) { result in
+            switch(result) {
+            case .success(let topArtists):
+                XCTAssertEqual(topArtists.pagination.page, 1)
+                XCTAssertEqual(topArtists.pagination.perPage, 5)
+                XCTAssertEqual(topArtists.pagination.total, 1297046)
+                XCTAssertEqual(topArtists.pagination.totalPages, 259410)
+
+                XCTAssertEqual(topArtists.items.count, 5)
+                XCTAssertEqual(topArtists.items[0].name, "Radiohead")
+                XCTAssertEqual(topArtists.items[0].listeners, 6214938)
+                XCTAssertEqual(topArtists.items[0].mbid, "a74b1b7f-71a5-4011-9441-d0b5e4122711")
+                XCTAssertEqual(topArtists.items[0].url.absoluteString, "https://www.last.fm/music/Radiohead")
+                XCTAssertEqual(topArtists.items[0].streamable, false)
+
+                XCTAssertEqual(
+                    topArtists.items[0].images.small!.absoluteString,
+                    "https://lastfm.freetls.fastly.net/i/u/34s/2a96cbd8b46e442fc41c2b86b821562f.png"
+                )
+
+                XCTAssertEqual(
+                    topArtists.items[0].images.medium!.absoluteString,
+                    "https://lastfm.freetls.fastly.net/i/u/64s/2a96cbd8b46e442fc41c2b86b821562f.png"
+                )
+
+                XCTAssertEqual(
+                    topArtists.items[0].images.large!.absoluteString,
+                    "https://lastfm.freetls.fastly.net/i/u/174s/2a96cbd8b46e442fc41c2b86b821562f.png"
+                )
+
+                XCTAssertEqual(
+                    topArtists.items[0].images.extraLarge!.absoluteString,
+                    "https://lastfm.freetls.fastly.net/i/u/300x300/2a96cbd8b46e442fc41c2b86b821562f.png"
+                )
+
+                XCTAssertEqual(
+                    topArtists.items[0].images.mega!.absoluteString,
+                    "https://lastfm.freetls.fastly.net/i/u/300x300/2a96cbd8b46e442fc41c2b86b821562f.png"
+                )
+            case .failure(let error):
+                XCTFail("It was expected to succeed, but it failed with error \(error.localizedDescription)")
+            }
+
+            expectation.fulfill()
+        }
+
+        waitForExpectations(timeout: 3)
+    }
 }
