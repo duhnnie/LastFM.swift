a9eefb29d1ddc38cfaba8db4fdd6a06fdf3879a9
Daniel <dnl.canedo@gmail.com>
2023-10-23 22:12:13 -0400

    LF-35 Add chart->getTopArtists

---
+++ Sources/LastFM/Models/ChartTopArtist.swift
@@ -0,0 +1,45 @@
+import Foundation
+
+public struct ChartTopArtist: Decodable, Equatable {
+    public let mbid: String
+    public let name: String
+    public let images: LastFMImages
+    public let url: URL
+    public let listeners: UInt
+    public let playcount: UInt
+    public let streamable: Bool
+
+    private enum CodingKeys: String, CodingKey {
+        case mbid
+        case name
+        case images = "image"
+        case url
+        case listeners
+        case playcount
+        case streamable
+    }
+
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+
+        self.mbid = try container.decode(String.self, forKey: CodingKeys.mbid)
+        self.name = try container.decode(String.self, forKey: CodingKeys.name)
+        self.images = try container.decode(LastFMImages.self, forKey: CodingKeys.images)
+        self.url = try container.decode(URL.self, forKey: .url)
+
+        let listenersString = try container.decode(String.self, forKey: CodingKeys.listeners)
+        let playcountString = try container.decode(String.self, forKey: CodingKeys.playcount)
+        let streamableString = try container.decode(String.self, forKey: CodingKeys.streamable)
+
+        guard
+            let listeners = UInt(listenersString),
+            let playcount = UInt(playcountString)
+        else {
+            throw RuntimeError("Can't parse playcount or listeners")
+        }
+
+        self.playcount = playcount
+        self.listeners = listeners
+        self.streamable = streamableString == "1" ? true : false
+    }
+}
--- Sources/LastFM/Modules/ChartModule.swift
+++ Sources/LastFM/Modules/ChartModule.swift
@@ -4,6 +4,7 @@ public struct ChartModule {
 
     internal enum APIMethod: String, MethodKey {
         case getTopTracks = "gettoptracks"
+        case getTopArtists = "gettopartists"
 
         func getName() -> String {
             return "chart.\(self.rawValue)"
@@ -23,6 +24,15 @@ public struct ChartModule {
         onCompletion: @escaping LastFM.OnCompletion<CollectionPage<ChartTopTrack>>
     ) {
         let params = instance.normalizeParams(params: params, method: APIMethod.getTopTracks)
+
+        requester.getDataAndParse(params: params, secure: false, onCompletion: onCompletion)
+    }
+
+    public func getTopArtists(
+        params: ChartTopArtistsParams,
+        onCompletion: @escaping LastFM.OnCompletion<CollectionPage<ChartTopArtist>>
+    ) {
+        let params = instance.normalizeParams(params: params, method: APIMethod.getTopArtists)
 
         requester.getDataAndParse(params: params, secure: false, onCompletion: onCompletion)
     }
