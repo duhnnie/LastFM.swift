9ccc2c5aecf72dd8d58c998999fe33b7ba837772
Daniel <dnl.canedo@gmail.com>
2023-11-05 15:53:33 -0400

    LF-84 Extend container decoder to allow decoding Int, UInt, Bool from String

---
+++ Sources/LastFM/Extensions/KeyedDecodingContainer+StringDecoding.swift
@@ -0,0 +1,172 @@
+import Foundation
+
+fileprivate struct UIntWrapper: Decodable {
+
+    let value: UInt
+
+    init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+
+        if let value = try? container.decode(UInt.self) {
+            self.value = value
+            return
+        }
+
+        guard
+            let valueString = try? container.decode(String.self),
+            let value = UInt(valueString)
+        else {
+            let context = DecodingError.Context(codingPath: container.codingPath, debugDescription: "UInt format was not parseable.")
+            throw DecodingError.dataCorrupted(context)
+        }
+
+        self.value = value
+    }
+
+}
+
+fileprivate struct IntWrapper: Decodable {
+
+    let value: Int
+
+    init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+
+        if let value = try? container.decode(Int.self) {
+            self.value = value
+            return
+        }
+
+        guard
+            let valueString = try? container.decode(String.self),
+            let value = Int(valueString)
+        else {
+            let context = DecodingError.Context(
+                codingPath: container.codingPath,
+                debugDescription: "Int format was not parseable."
+            )
+
+            throw DecodingError.dataCorrupted(context)
+        }
+
+        self.value = value
+    }
+
+}
+
+fileprivate struct FloatWrapper: Decodable {
+
+    let value: Float
+
+    init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+
+        if let value = try? container.decode(Float.self) {
+            self.value = value
+            return
+        }
+
+        guard
+            let valueString = try? container.decode(String.self),
+            let value = Float(valueString)
+        else {
+            let context = DecodingError.Context(
+                codingPath: container.codingPath,
+                debugDescription: "Float format was not parseable."
+            )
+
+            throw DecodingError.dataCorrupted(context)
+        }
+
+        self.value = value
+    }
+
+}
+
+fileprivate struct BoolWrapper: Decodable {
+
+    let value: Bool
+
+    init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+
+        if let value = try? container.decode(Bool.self) {
+            self.value = value
+            return
+        }
+
+        guard
+            let valueString = try? container.decode(String.self)
+        else {
+            let context = DecodingError.Context(
+                codingPath: container.codingPath,
+                debugDescription: "Bool format was not parseable."
+            )
+
+            throw DecodingError.dataCorrupted(context)
+        }
+
+        self.value = !["false", "0", ""].contains(valueString.lowercased())
+    }
+
+}
+
+internal extension KeyedDecodingContainer {
+
+    func decode(_ type: UInt.Type, forKey key: K) throws -> UInt {
+        return try self.decode(UIntWrapper.self, forKey: key).value
+    }
+
+    func decode(_ type: Int.Type, forKey key: K) throws -> Int {
+        return try self.decode(IntWrapper.self, forKey: key).value
+    }
+
+    func decode(_ type: Float.Type, forKey key: K) throws -> Float {
+        return try self.decode(FloatWrapper.self, forKey: key).value
+    }
+
+    func decode(_ type: Bool.Type, forKey key: K) throws -> Bool {
+        return try self.decode(BoolWrapper.self, forKey: key).value
+    }
+
+    func decodeIfPresent(_ type: Int.Type, forKey key: K) throws -> Int? {
+        guard
+            (self.allKeys.contains { $0.stringValue == key.stringValue })
+        else {
+            return nil
+        }
+
+        return try decode(type, forKey: key)
+    }
+
+    func decodeIfPresent(_ type: UInt.Type, forKey key: K) throws -> UInt? {
+        guard
+            (self.allKeys.contains { $0.stringValue == key.stringValue })
+        else {
+            return nil
+        }
+
+        return try decode(type, forKey: key)
+    }
+
+    func decodeIfPresent(_ type: Float.Type, forKey key: K) throws -> Float? {
+        guard
+            (self.allKeys.contains { $0.stringValue == key.stringValue })
+        else {
+            return nil
+        }
+
+        return try decode(type, forKey: key)
+    }
+
+    func decodeIfPresent(_ type: Bool.Type, forKey key: K) throws -> Bool? {
+        guard
+            (self.allKeys.contains { $0.stringValue == key.stringValue })
+        else {
+            return nil
+        }
+
+        return try decode(type, forKey: key)
+    }
+    
+}
