0de2b0627dcdd0d552a1e5074335c5159e9e3e28
Daniel <dnl.canedo@gmail.com>
2023-11-13 22:46:09 -0400

    LF-40 Add unit test for auth->getToken()

--- Tests/LastFMTests/Modules/AuthModuleTests.swift
+++ Tests/LastFMTests/Modules/AuthModuleTests.swift
@@ -59,4 +59,39 @@ class AuthModuleTests: XCTestCase {
         )
     }
 
+    func test_getToken_success() throws {
+        let jsonURL = Bundle.module.url(
+            forResource: "Resources/auth.getToken",
+            withExtension: "json"
+        )!
+
+        let fakeData = try Data(contentsOf: jsonURL)
+        let expectation = expectation(description: "Waiting for getToken()")
+
+        apiClient.data = fakeData
+        apiClient.response = Constants.RESPONSE_200_OK
+
+        try instance.getToken { result in
+            expectation.fulfill()
+
+            switch (result) {
+            case .success(let token):
+                XCTAssertEqual(token, "0987654321")
+            case .failure(let error):
+                XCTFail("It was expected to succeed, but it failed. Error: \(error.localizedDescription)")
+            }
+        }
+
+        waitForExpectations(timeout: 3)
+        XCTAssertEqual(apiClient.getCalls.count, 1)
+        XCTAssertEqual(apiClient.getCalls[0].headers, nil)
+
+        XCTAssertTrue(
+            Util.areSameURL(
+                apiClient.getCalls[0].url.absoluteString,
+                "http://ws.audioscrobbler.com/2.0?format=json&api_sig=122ce70950973a4b1f1d8c59030fa108&method=auth.gettoken&api_key=someAPIKey"
+            )
+        )
+    }
+
 }
