6adf59d6021e9080b43c147bced607c7be99b6c1
Daniel <dnl.canedo@gmail.com>
2023-11-14 19:10:55 -0400

    LF-51 Add album->addTags()

---
+++ Sources/LastFM/Models/Params/AlbumTagsParams.swift
@@ -0,0 +1,23 @@
+import Foundation
+
+public struct AlbumTagsParams: Params {
+
+    public var artist: String
+    public var album: String
+    public var tags: [String]
+
+    public init(artist: String, album: String, tags: [String]) {
+        self.artist = artist
+        self.album = album
+        self.tags = tags
+    }
+
+    internal func toDictionary() -> Dictionary<String, String> {
+        return [
+            "artist": self.artist,
+            "album": self.album,
+            "tags": self.tags.joined(separator: ",")
+        ]
+    }
+
+}
