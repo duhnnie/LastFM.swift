222d8b6289c29536a66f28adcdea62fdb1e721e0
Daniel <dnl.canedo@gmail.com>
2023-11-23 12:03:04 -0400

    LF-36 Add chart->getTopTags()

--- Sources/LastFM/Modules/ChartModule.swift
+++ Sources/LastFM/Modules/ChartModule.swift
@@ -5,6 +5,7 @@ public struct ChartModule {
     internal enum APIMethod: String, MethodKey {
         case getTopTracks = "gettoptracks"
         case getTopArtists = "gettopartists"
+        case getTopTags = "gettoptags"
 
         func getName() -> String {
             return "chart.\(self.rawValue)"
@@ -33,6 +34,21 @@ public struct ChartModule {
         onCompletion: @escaping LastFM.OnCompletion<CollectionPage<ChartTopArtist>>
     ) {
         let params = parent.normalizeParams(params: params, method: APIMethod.getTopArtists)
+
+        requester.getDataAndParse(params: params, secure: false, onCompletion: onCompletion)
+    }
+
+    public func getTopTags(
+        page: UInt,
+        limit: UInt,
+        onCompletion: @escaping LastFM.OnCompletion<CollectionPage<ChartTopTag>>
+    ) {
+        let params = parent.normalizeParams(
+            params: [
+                "page": String(page),
+                "limit": String(limit)
+            ], method: APIMethod.getTopTags
+        )
 
         requester.getDataAndParse(params: params, secure: false, onCompletion: onCompletion)
     }
