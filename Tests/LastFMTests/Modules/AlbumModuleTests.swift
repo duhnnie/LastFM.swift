cd807044800fd7d7ca251858c8f6a35f3a594a42
Daniel <dnl.canedo@gmail.com>
2023-11-22 22:29:13 -0400

    LF-53 Add unit tests for album->getTags()

--- Tests/LastFMTests/Modules/AlbumModuleTests.swift
+++ Tests/LastFMTests/Modules/AlbumModuleTests.swift
@@ -364,7 +364,7 @@ class AlbumModuleTests: XCTestCase {
     func test_addTags_success() throws {
         let expectation = expectation(description: "Waiting for addTags()")
 
-        let params = AlbumTagsParams(
+        let params = AlbumAddTagsParams(
             artist: "Some Artist",
             album: "Some Album",
             tags: ["tag 1", "tag-2", "tag3"]
@@ -445,6 +445,95 @@ class AlbumModuleTests: XCTestCase {
             Util.areSameURL(
                 "https://domain.com/?" + payloadString,
                 "https://domain.com/?" + expectedPayload
+            )
+        )
+    }
+
+    func test_getTags_success() throws {
+        let jsonURL = Bundle.module.url(
+            forResource: "Resources/track.getTags",
+            withExtension: "json"
+        )!
+
+        let fakeData = try Data(contentsOf: jsonURL)
+        let expectation = expectation(description: "Waiting for getTags()")
+
+        let params = AlbumGetTagsParams(
+            artist: "Some Artist",
+            album: "Some Album",
+            autocorrect: true,
+            user: "pepiro"
+        )
+
+        apiClient.data = fakeData
+        apiClient.response = Constants.RESPONSE_200_OK
+
+        instance.getTags(params: params) { result in
+            switch (result) {
+            case .success(let tags):
+                XCTAssertEqual(tags.items.count, 2)
+
+                XCTAssertEqual(tags.items[0].name, "tag-1")
+                XCTAssertEqual(tags.items[0].url.absoluteString, "https://tags.com/tag-1")
+
+                XCTAssertEqual(tags.items[1].name, "tag-2")
+                XCTAssertEqual(tags.items[1].url.absoluteString, "https://tags.com/tag-2")
+            case .failure(let error):
+                XCTFail("Expected to succeed, but it failed. Error: \(error)")
+            }
+
+            expectation.fulfill()
+        }
+
+        waitForExpectations(timeout: 3)
+        XCTAssertEqual(apiClient.getCalls.count, 1)
+        XCTAssertEqual(apiClient.getCalls[0].headers, nil)
+
+        XCTAssertTrue(
+            Util.areSameURL(
+                apiClient.getCalls[0].url.absoluteString,
+                "http://ws.audioscrobbler.com/2.0?artist=Some%20Artist&album=Some%20Album&autocorrect=1&user=pepiro&method=album.gettags&api_key=someAPIKey&format=json"
+            )
+        )
+    }
+
+    func test_getTagsByMBID_success() throws {
+        let jsonURL = Bundle.module.url(
+            forResource: "Resources/track.getTags",
+            withExtension: "json"
+        )!
+
+        let fakeData = try Data(contentsOf: jsonURL)
+        let expectation = expectation(description: "Waiting for getTags()")
+
+        let params = InfoByMBIDParams(
+            mbid: "some-artist-mbid",
+            autocorrect: true,
+            username: "pepiro"
+        )
+
+        apiClient.data = fakeData
+        apiClient.response = Constants.RESPONSE_200_OK
+
+        instance.getTags(params: params) { result in
+            switch (result) {
+            case .success(_):
+                break
+            case .failure(let error):
+                XCTFail("Expected to succeed, but it failed. Error: \(error)")
+            }
+
+            expectation.fulfill()
+        }
+
+        waitForExpectations(timeout: 3)
+        XCTAssertEqual(apiClient.getCalls.count, 1)
+        XCTAssertEqual(apiClient.getCalls[0].headers, nil)
+
+        XCTAssertTrue(
+            Util.areSameURL(
+                apiClient.getCalls[0].url.absoluteString,
+                "http://ws.audioscrobbler.com/2.0?mbid=some-artist-mbid&autocorrect=1&user=pepiro&method=album.gettags&api_key=someAPIKey&format=json"
             )
         )
     }
