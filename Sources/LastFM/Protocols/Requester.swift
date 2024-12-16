58f4284c9e8ad8d8796fbe1bc8f1ec4db38fb72e
Daniel Canedo <dnl.canedo@gmail.com>
2024-12-16 14:50:13 -0400

    LF-109 Update Requester for supporting async/await

--- Sources/LastFM/Protocols/Requester.swift
+++ Sources/LastFM/Protocols/Requester.swift
@@ -2,7 +2,14 @@ import Foundation
 import SwiftRestClient
 
 internal protocol Requester {
-
+    
+    @available(iOS 13.0, watchOS 6.0, tvOS 13.0, *)
+    func getDataAndParse<T: Decodable>(
+        params: [String: String],
+        type: T.Type,
+        secure: Bool
+    ) async throws -> T
+    
     func getDataAndParse<T: Decodable>(
         params: [String: String],
         secure: Bool,
@@ -14,6 +21,12 @@ internal protocol Requester {
         secure: Bool,
         onCompletion: @escaping LastFM.OnCompletion<T>
     ) throws
+    
+    @available(iOS 13.0, watchOS 6.0, tvOS 13.0, *)
+    func postFormURLEncoded(
+        payload: [String: String],
+        secure: Bool
+    ) async throws
 
     func postFormURLEncoded(
         payload: [String: String],
