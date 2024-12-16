e35dc8a0ea4e8c45ce87685160a8354699a90be6
Daniel Canedo <dnl.canedo@gmail.com>
2024-12-16 14:49:15 -0400

    LF-109 Update apiclient protocols to support async/await

--- Sources/LastFM/Protocols/APIClient.swift
+++ Sources/LastFM/Protocols/APIClient.swift
@@ -2,12 +2,25 @@ import Foundation
 import SwiftRestClient
 
 internal protocol APIClient {
+    
+    @available(iOS 13.0, watchOS 6.0, tvOS 13.0, *)
+    func get(
+        _ url: URL,
+        headers: SwiftRestClient.Headers?
+    ) async throws -> (Data, URLResponse)
 
     func get(
         _ url: URL,
         headers: SwiftRestClient.Headers?,
         onCompletion: @escaping SwiftRestClient.RequestCompletion
     )
+    
+    @available(iOS 13.0, watchOS 6.0, tvOS 13.0, *)
+    func post(
+        _ url: URL,
+        body: Data?,
+        headers: SwiftRestClient.Headers?
+    ) async throws -> (Data, URLResponse)
 
     func post(
         _ url: URL,
