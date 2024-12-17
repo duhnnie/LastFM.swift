795d9add2a9404083f60880197c1b909009cbc35
Daniel Canedo <dnl.canedo@gmail.com>
2024-12-16 23:57:28 -0400

    LF-109 Make ArtistModule support async/await

--- Sources/LastFM/Modules/ArtistModule.swift
+++ Sources/LastFM/Modules/ArtistModule.swift
@@ -37,12 +37,36 @@ public struct ArtistModule {
 
         requester.getDataAndParse(params: params, secure: self.secure, onCompletion: onCompletion)
     }
+    
+    @available(iOS 13.0, watchOS 6.0, tvOS 13.0, *)
+    private func internalGetTopTracks(params: Params) async throws -> CollectionPage<ArtistTopTrack> {
+        return try await withCheckedThrowingContinuation({ continuation in
+            self.internalGetTopTracks(params: params) { result in
+                switch (result) {
+                case .success(let artistTopTracks):
+                    continuation.resume(returning: artistTopTracks)
+                case .failure(let error):
+                    continuation.resume(throwing: error)
+                }
+            }
+        })
+    }
+    
+    @available(iOS 13.0, watchOS 6.0, tvOS 13.0, *)
+    public func getTopTracks(params: ArtistTopItemsParams) async throws -> CollectionPage<ArtistTopTrack> {
+        return try await self.internalGetTopTracks(params: params)
+    }
 
     public func getTopTracks(
         params: ArtistTopItemsParams,
         onCompletion: @escaping LastFM.OnCompletion<CollectionPage<ArtistTopTrack>>
     ) {
         self.internalGetTopTracks(params: params, onCompletion: onCompletion)
+    }
+    
+    @available(iOS 13.0, watchOS 6.0, tvOS 13.0, *)
+    public func getTopTracks(params: MBIDPageParams) async throws -> CollectionPage<ArtistTopTrack> {
+        return try await self.internalGetTopTracks(params: params)
     }
 
     public func getTopTracks(
@@ -60,6 +84,22 @@ public struct ArtistModule {
 
         requester.getDataAndParse(params: params, secure: self.secure, onCompletion: onCompletion)
     }
+    
+    @available(iOS 13.0, watchOS 6.0, tvOS 13.0, *)
+    private func internalGetSimilar(params: Params) async throws -> CollectionList<ArtistSimilar> {
+        let params = parent.normalizeParams(params: params, method: APIMethod.getSimilar)
+        
+        return try await requester.getDataAndParse(
+            params: params,
+            type: CollectionList<ArtistSimilar>.self,
+            secure: self.secure
+        )
+    }
+    
+    @available(iOS 13.0, watchOS 6.0, tvOS 13.0, *)
+    public func getSimilar(params: ArtistSimilarParams) async throws -> CollectionList<ArtistSimilar> {
+        return try await self.internalGetSimilar(params: params)
+    }
 
     public func getSimilar(
         params: ArtistSimilarParams,
@@ -67,12 +107,31 @@ public struct ArtistModule {
     ) {
         self.internalGetSimilar(params: params, onCompletion: onCompletion)
     }
+    
+    @available(iOS 13.0, watchOS 6.0, tvOS 13.0, *)
+    public func getSimilar(params: MBIDListParams) async throws -> CollectionList<ArtistSimilar> {
+        return try await self.internalGetSimilar(params: params)
+    }
 
     public func getSimilar(
         params: MBIDListParams,
         onCompletion: @escaping LastFM.OnCompletion<CollectionList<ArtistSimilar>>
     ) {
         self.internalGetSimilar(params: params, onCompletion: onCompletion)
+    }
+    
+    @available(iOS 13.0, watchOS 6.0, tvOS 13.0, *)
+    public func search(params: SearchParams) async throws -> SearchResults<ArtistSearchResult> {
+        let params = parent.normalizeParams(
+            params: params.toDictionary(termKey: "artist"),
+            method: APIMethod.search
+        )
+
+        return try await requester.getDataAndParse(
+            params: params,
+            type: SearchResults<ArtistSearchResult>.self,
+            secure: self.secure
+        )
     }
 
     public func search(
@@ -86,6 +145,17 @@ public struct ArtistModule {
 
         requester.getDataAndParse(params: params, secure: self.secure, onCompletion: onCompletion)
     }
+    
+    @available(iOS 13.0, watchOS 6.0, tvOS 13.0, *)
+    private func internalGetTopAlbums(params: Params) async throws -> CollectionPage<ArtistTopAlbum> {
+        let params = parent.normalizeParams(params: params, method: APIMethod.getTopAlbums)
+
+        return try await requester.getDataAndParse(
+            params: params,
+            type: CollectionPage<ArtistTopAlbum>.self,
+            secure: self.secure
+        )
+    }
 
     private func internalGetTopAlbums(
         params: Params,
@@ -95,6 +165,11 @@ public struct ArtistModule {
 
         requester.getDataAndParse(params: params, secure: self.secure, onCompletion: onCompletion)
     }
+    
+    @available(iOS 13.0, watchOS 6.0, tvOS 13.0, *)
+    public func getTopAlbums(params: ArtistTopItemsParams) async throws -> CollectionPage<ArtistTopAlbum> {
+        return try await internalGetTopAlbums(params: params)
+    }
 
     public func getTopAlbums(
         params: ArtistTopItemsParams,
@@ -102,12 +177,26 @@ public struct ArtistModule {
     ) {
         self.internalGetTopAlbums(params: params, onCompletion: onCompletion)
     }
+    
+    @available(iOS 13.0, watchOS 6.0, tvOS 13.0, *)
+    public func getTopAlbums(params: MBIDPageParams) async throws -> CollectionPage<ArtistTopAlbum> {
+        return try await internalGetTopAlbums(params: params)
+    }
 
     public func getTopAlbums(
         params: MBIDPageParams,
         onCompletion: @escaping LastFM.OnCompletion<CollectionPage<ArtistTopAlbum>>
     ) {
         self.internalGetTopAlbums(params: params, onCompletion: onCompletion)
+    }
+    
+    @available(iOS 13.0, watchOS 6.0, tvOS 13.0, *)
+    public func getInfo(
+        params: ArtistInfoParams
+    ) async throws -> ArtistInfo {
+        let params = parent.normalizeParams(params: params, method: APIMethod.getInfo)
+
+        return try await requester.getDataAndParse(params: params, type: ArtistInfo.self, secure: self.secure)
     }
 
     public func getInfo(
@@ -117,6 +206,27 @@ public struct ArtistModule {
         let params = parent.normalizeParams(params: params, method: APIMethod.getInfo)
 
         requester.getDataAndParse(params: params, secure: self.secure, onCompletion: onCompletion)
+    }
+    
+    @available(iOS 13.0, watchOS 6.0, tvOS 13.0, *)
+    public func addTags(
+        artist: String,
+        tags: [String],
+        sessionKey: String
+    ) async throws {
+        return try await withCheckedThrowingContinuation({ continuation in
+            do {
+                try self.addTags(artist: artist, tags: tags, sessionKey: sessionKey) { error in
+                    if let error = error {
+                        continuation.resume(throwing: error)
+                    } else {
+                        continuation.resume(returning: ())
+                    }
+                }
+            } catch {
+                continuation.resume(throwing: error)
+            }
+        })
     }
 
     public func addTags(
@@ -137,6 +247,27 @@ public struct ArtistModule {
         try parent.addSignature(params: &params)
         try requester.postFormURLEncoded(payload: params, secure: self.secure, onCompletion: onCompletion)
     }
+    
+    @available(iOS 13.0, watchOS 6.0, tvOS 13.0, *)
+    public func removeTag(
+        artist: String,
+        tag: String,
+        sessionKey: String
+    ) async throws {
+        return try await withCheckedThrowingContinuation({ continuation in
+            do {
+                try self.removeTag(artist: artist, tag: tag, sessionKey: sessionKey) { error in
+                    if let error = error {
+                        continuation.resume(throwing: error)
+                    } else {
+                        continuation.resume(returning: ())
+                    }
+                }
+            } catch {
+                continuation.resume(throwing: error)
+            }
+        })
+    }
 
     public func removeTag(
         artist: String,
@@ -156,6 +287,20 @@ public struct ArtistModule {
         try parent.addSignature(params: &params)
         try requester.postFormURLEncoded(payload: params, secure: self.secure, onCompletion: onCompletion)
     }
+    
+    @available(iOS 13.0, watchOS 6.0, tvOS 13.0, *)
+    public func getCorrection(artist: String) async throws -> ArtistCorrection {
+        return try await withCheckedThrowingContinuation({ continuation in
+            self.getCorrection(artist: artist) { result in
+                switch (result) {
+                case .success(let artistCorrection):
+                    continuation.resume(returning: artistCorrection)
+                case .failure(let error):
+                    continuation.resume(throwing: error)
+                }
+            }
+        })
+    }
 
     public func getCorrection(
         artist: String,
@@ -168,6 +313,13 @@ public struct ArtistModule {
 
         requester.getDataAndParse(params: params, secure: self.secure, onCompletion: onCompletion)
     }
+    
+    @available(iOS 13.0, watchOS 6.0, tvOS 13.0, *)
+    public func getTags(params: ArtistTagsParams) async throws -> CollectionList<LastFMEntity> {
+        let params = parent.normalizeParams(params: params, method: APIMethod.getTags)
+
+        return try await requester.getDataAndParse(params: params, type: CollectionList<LastFMEntity>.self, secure: self.secure)
+    }
 
     public func getTags(
         params: ArtistTagsParams,
@@ -176,6 +328,20 @@ public struct ArtistModule {
         let params = parent.normalizeParams(params: params, method: APIMethod.getTags)
 
         requester.getDataAndParse(params: params, secure: self.secure, onCompletion: onCompletion)
+    }
+    
+    @available(iOS 13.0, watchOS 6.0, tvOS 13.0, *)
+    public func getTags(params: InfoByMBIDParams) async throws -> CollectionList<LastFMEntity> {
+        return try await withCheckedThrowingContinuation({ continuation in
+            self.getTags(params: params) { result in
+                switch (result) {
+                case .success(let tags):
+                    continuation.resume(returning: tags)
+                case .failure(let error):
+                    continuation.resume(throwing: error)
+                }
+            }
+        })
     }
 
     public func getTags(
@@ -193,6 +359,20 @@ public struct ArtistModule {
 
         requester.getDataAndParse(params: params, secure: self.secure, onCompletion: onCompletion)
     }
+    
+    @available(iOS 13.0, watchOS 6.0, tvOS 13.0, *)
+    public func getTopTags(artist: String, autocorrect: Bool = true) async throws -> CollectionList<TopTag> {
+        return try await withCheckedThrowingContinuation({ continuation in
+            self.getTopTags(artist: artist) { result in
+                switch (result) {
+                case .success(let topTags):
+                    continuation.resume(returning: topTags)
+                case .failure(let error):
+                    continuation.resume(throwing: error)
+                }
+            }
+        })
+    }
 
     public func getTopTags(
         artist: String,
@@ -208,6 +388,20 @@ public struct ArtistModule {
         )
 
         requester.getDataAndParse(params: params, secure: self.secure, onCompletion: onCompletion)
+    }
+    
+    @available(iOS 13.0, watchOS 6.0, tvOS 13.0, *)
+    public func getTopTags(mbid: String, autocorrect: Bool = true) async throws -> CollectionList<TopTag> {
+        return try await withCheckedThrowingContinuation({ continuation in
+            self.getTopTags(mbid: mbid, autocorrect: autocorrect) { result in
+                switch result {
+                case .success(let topTags):
+                    continuation.resume(returning: topTags)
+                case .failure(let error):
+                    continuation.resume(throwing: error)
+                }
+            }
+        })
     }
 
     public func getTopTags(
