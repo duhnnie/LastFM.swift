0a6504daca1b1fd8b4ad567b33711b03493da370
Daniel Canedo <dnl.canedo@gmail.com>
2024-12-17 13:37:39 -0400

    LF-109 Make ChartModule support async/await

--- Sources/LastFM/Modules/ChartModule.swift
+++ Sources/LastFM/Modules/ChartModule.swift
@@ -22,6 +22,13 @@ public struct ChartModule {
         self.secure = secure
     }
 
+    @available(iOS 13.0, watchOS 6.0, tvOS 13.0, *)
+    public func getTopTracks(params: ChartTopItemsParams) async throws -> ChartTopTrack {
+        let params = parent.normalizeParams(params: params, method: APIMethod.getTopTracks)
+
+        return try await requester.getDataAndParse(params: params, type: ChartTopTrack.self, secure: self.secure)
+    }
+
     public func getTopTracks(
         params: ChartTopItemsParams,
         onCompletion: @escaping LastFM.OnCompletion<CollectionPage<ChartTopTrack>>
@@ -29,6 +36,13 @@ public struct ChartModule {
         let params = parent.normalizeParams(params: params, method: APIMethod.getTopTracks)
 
         requester.getDataAndParse(params: params, secure: self.secure, onCompletion: onCompletion)
+    }
+    
+    @available(iOS 13.0, watchOS 6.0, tvOS 13.0, *)
+    public func getTopArtists(params: ChartTopItemsParams) async throws -> ChartTopArtist {
+        let params = parent.normalizeParams(params: params, method: APIMethod.getTopArtists)
+
+        return try await requester.getDataAndParse(params: params, type: ChartTopArtist.self, secure: self.secure)
     }
 
     public func getTopArtists(
@@ -38,6 +52,20 @@ public struct ChartModule {
         let params = parent.normalizeParams(params: params, method: APIMethod.getTopArtists)
 
         requester.getDataAndParse(params: params, secure: self.secure, onCompletion: onCompletion)
+    }
+    
+    @available(iOS 13.0, watchOS 6.0, tvOS 13.0, *)
+    public func getTopTags(page: UInt, limit: UInt) async throws -> CollectionPage<ChartTopTag> {
+        return try await withCheckedThrowingContinuation({ continuation in
+            self.getTopTags(page: page, limit: limit) { result in
+                switch(result) {
+                case .success(let topTags):
+                    continuation.resume(returning: topTags)
+                case .failure(let error):
+                    continuation.resume(throwing: error)
+                }
+            }
+        })
     }
 
     public func getTopTags(
