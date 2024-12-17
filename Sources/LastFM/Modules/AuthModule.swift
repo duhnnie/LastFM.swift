fd91a89b657be0304e3fd5039fed561a371c3b0d
Daniel Canedo <dnl.canedo@gmail.com>
2024-12-17 13:05:56 -0400

    LF-109 Make AuthModule support async/await

--- Sources/LastFM/Modules/AuthModule.swift
+++ Sources/LastFM/Modules/AuthModule.swift
@@ -20,6 +20,18 @@ public struct AuthModule {
         self.parent = parent
         self.requester = requester
     }
+    
+    @available(iOS 13.0, watchOS 6.0, tvOS 13.0, *)
+    public func getSession(token: String) async throws -> ServiceSession {
+        var params = parent.normalizeParams(
+            params: ["token": token],
+            method: APIMethod.getSession
+        )
+
+        try parent.addSignature(params: &params)
+        
+        return try await requester.getDataAndParse(params: params, type: ServiceSession.self, secure: true)
+    }
 
     public func getSession(token: String, onCompletion: @escaping LastFM.OnCompletion<ServiceSession>) throws {
         var params = parent.normalizeParams(
@@ -30,6 +42,24 @@ public struct AuthModule {
         try parent.addSignature(params: &params)
 
         requester.getDataAndParse(params: params, secure: true, onCompletion: onCompletion)
+    }
+    
+    @available(iOS 13.0, watchOS 6.0, tvOS 13.0, *)
+    public func getToken() async throws -> String {
+        return try await withCheckedThrowingContinuation({ continuation in
+            do {
+                try self.getToken { result in
+                    switch(result) {
+                    case .success(let token):
+                        continuation.resume(returning: token)
+                    case .failure(let error):
+                        continuation.resume(throwing: error)
+                    }
+                }
+            } catch {
+                continuation.resume(throwing: error)
+            }
+        })
     }
 
     public func getToken(onCompletion: @escaping LastFM.OnCompletion<String>) throws {
@@ -51,6 +81,25 @@ public struct AuthModule {
             secure: true,
             onCompletion: internalOnCompletion
         )
+    }
+    
+    @available(iOS 13.0, watchOS 6.0, tvOS 13.0, *)
+    @available(*, deprecated, message: "Last.FM: This method has other parameters which are now deprecated and should not be used.")
+    public func getMobileSession(username: String, password: String) async throws -> ServiceSession {
+        return try await withCheckedThrowingContinuation({ continuation in
+            do {
+                try self.getMobileSession(username: username, password: password) { result in
+                    switch (result) {
+                    case .success(let serviceSession):
+                        continuation.resume(returning: serviceSession)
+                    case .failure(let error):
+                        continuation.resume(throwing: error)
+                    }
+                }
+            } catch {
+                continuation.resume(throwing: error)
+            }
+        })
     }
 
     @available(*, deprecated, message: "Last.FM: This method has other parameters which are now deprecated and should not be used.")
