5e6d2a2aa13bb41ff0c74ccfad2dd9ca400ed93d
Daniel Canedo <dnl.canedo@gmail.com>
2024-12-21 17:11:19 -0400

    LF-109 Update tests for GeoModule

--- Tests/LastFMTests/Modules/GeoModuleTests.swift
+++ Tests/LastFMTests/Modules/GeoModuleTests.swift
@@ -23,7 +23,115 @@ class GeoModuleTests: XCTestCase {
         apiClient.clearMock()
     }
 
+    private func validateGeoTopTracks(_ topTracks: CollectionPage<GeoTopTrack>) {
+        XCTAssertEqual(topTracks.items.count, 2)
+        XCTAssertEqual(topTracks.items[0].name, "Track 0")
+        XCTAssertEqual(topTracks.items[0].duration, 100)
+        XCTAssertEqual(topTracks.items[0].listeners, 900)
+        XCTAssertEqual(topTracks.items[0].mbid, "track-0-mbid")
+        XCTAssertEqual(topTracks.items[0].url.absoluteString, "https://tracks.com/track-0")
+        XCTAssertEqual(topTracks.items[0].streamable, .noStreamable)
+        XCTAssertEqual(topTracks.items[0].artist.name, "Artist 0")
+        XCTAssertEqual(topTracks.items[0].artist.mbid, "artist-0-mbid")
+        XCTAssertEqual(topTracks.items[0].artist.url.absoluteString, "https://artist.com/artist-0")
+        XCTAssertEqual(topTracks.items[0].image.small?.absoluteString, "https://images.com/artist-0-s.png")
+        XCTAssertEqual(topTracks.items[0].image.medium?.absoluteString, "https://images.com/artist-0-m.png")
+        XCTAssertEqual(topTracks.items[0].image.large?.absoluteString, "https://images.com/artist-0-l.png")
+        XCTAssertEqual(topTracks.items[0].image.extraLarge?.absoluteString, "https://images.com/artist-0-xl.png")
+        XCTAssertEqual(topTracks.items[0].rank, 1)
+        XCTAssertEqual(topTracks.items[1].name, "Track 1")
+        XCTAssertEqual(topTracks.items[1].duration, 101)
+        XCTAssertEqual(topTracks.items[1].listeners, 910)
+        XCTAssertEqual(topTracks.items[1].mbid, "track-1-mbid")
+        XCTAssertEqual(topTracks.items[1].url.absoluteString, "https://tracks.com/track-1")
+        XCTAssertEqual(topTracks.items[1].streamable, .streamableFullTrack)
+        XCTAssertEqual(topTracks.items[1].artist.name, "Artist 1")
+        XCTAssertEqual(topTracks.items[1].artist.mbid, "artist-1-mbid")
+        XCTAssertEqual(topTracks.items[1].artist.url.absoluteString, "https://artist.com/artist-1")
+        XCTAssertEqual(topTracks.items[1].image.small?.absoluteString, "https://images.com/artist-1-s.png")
+        XCTAssertEqual(topTracks.items[1].image.medium?.absoluteString, "https://images.com/artist-1-m.png")
+        XCTAssertEqual(topTracks.items[1].image.large?.absoluteString, "https://images.com/artist-1-l.png")
+        XCTAssertEqual(topTracks.items[1].image.extraLarge?.absoluteString, "https://images.com/artist-1-xl.png")
+        XCTAssertEqual(topTracks.items[1].rank, 2)
+
+        XCTAssertEqual(topTracks.pagination.total, 500)
+        XCTAssertEqual(topTracks.pagination.totalPages, 100)
+        XCTAssertEqual(topTracks.pagination.perPage, 5)
+        XCTAssertEqual(topTracks.pagination.page, 1)
+    }
+
+    private func validateGeoTopArtists(_ topArtists: CollectionPage<GeoTopArtist>) {
+        XCTAssertEqual(topArtists.pagination.page, 1)
+        XCTAssertEqual(topArtists.pagination.perPage, 5)
+        XCTAssertEqual(topArtists.pagination.total, 1297046)
+        XCTAssertEqual(topArtists.pagination.totalPages, 259410)
+
+        XCTAssertEqual(topArtists.items.count, 5)
+        XCTAssertEqual(topArtists.items[0].name, "Radiohead")
+        XCTAssertEqual(topArtists.items[0].listeners, 6214938)
+        XCTAssertEqual(topArtists.items[0].mbid, "a74b1b7f-71a5-4011-9441-d0b5e4122711")
+        XCTAssertEqual(topArtists.items[0].url.absoluteString, "https://www.last.fm/music/Radiohead")
+        XCTAssertEqual(topArtists.items[0].streamable, false)
+
+        XCTAssertEqual(
+            topArtists.items[0].image.small!.absoluteString,
+            "https://lastfm.freetls.fastly.net/i/u/34s/2a96cbd8b46e442fc41c2b86b821562f.png"
+        )
+
+        XCTAssertEqual(
+            topArtists.items[0].image.medium!.absoluteString,
+            "https://lastfm.freetls.fastly.net/i/u/64s/2a96cbd8b46e442fc41c2b86b821562f.png"
+        )
+
+        XCTAssertEqual(
+            topArtists.items[0].image.large!.absoluteString,
+            "https://lastfm.freetls.fastly.net/i/u/174s/2a96cbd8b46e442fc41c2b86b821562f.png"
+        )
+
+        XCTAssertEqual(
+            topArtists.items[0].image.extraLarge!.absoluteString,
+            "https://lastfm.freetls.fastly.net/i/u/300x300/2a96cbd8b46e442fc41c2b86b821562f.png"
+        )
+
+        XCTAssertEqual(
+            topArtists.items[0].image.mega!.absoluteString,
+            "https://lastfm.freetls.fastly.net/i/u/300x300/2a96cbd8b46e442fc41c2b86b821562f.png"
+        )
+    }
+
     // getTopTracks
+
+    func test_getTopTracks_success() async throws {
+        let jsonURL = Bundle.module.url(
+            forResource: "Resources/geo.getTopTracks",
+            withExtension: "json"
+        )!
+
+        let fakeData = try Data(contentsOf: jsonURL)
+
+        let params = GeoTopTracksParams(
+            country: "Bolivia",
+            location: "somelocation",
+            limit: 5,
+            page: 1
+        )
+
+        apiClient.data = fakeData
+        apiClient.response = Constants.RESPONSE_200_OK
+
+        let topTracks = try await instance.getTopTracks(params: params)
+
+        validateGeoTopTracks(topTracks)
+
+        XCTAssertEqual(apiClient.asyncGetCalls.count, 1)
+        XCTAssertNil(apiClient.asyncGetCalls[0].headers)
+
+        XCTAssertTrue(
+            Util.areSameURL(
+                apiClient.asyncGetCalls[0].url.absoluteString,
+                "https://ws.audioscrobbler.com/2.0/?api_key=\(Constants.API_KEY)&format=json&method=geo.gettoptracks&country=\(params.country)&location=\(params.location!)&limit=\(params.limit)&page=\(params.page)")
+        )
+    }
 
     func test_getTopTracks_success() throws {
         let jsonURL = Bundle.module.url(
@@ -47,40 +155,7 @@ class GeoModuleTests: XCTestCase {
         instance.getTopTracks(params: params) { result in
             switch(result) {
             case .success(let topTracks):
-                XCTAssertEqual(topTracks.items.count, 2)
-                XCTAssertEqual(topTracks.items[0].name, "Track 0")
-                XCTAssertEqual(topTracks.items[0].duration, 100)
-                XCTAssertEqual(topTracks.items[0].listeners, 900)
-                XCTAssertEqual(topTracks.items[0].mbid, "track-0-mbid")
-                XCTAssertEqual(topTracks.items[0].url.absoluteString, "https://tracks.com/track-0")
-                XCTAssertEqual(topTracks.items[0].streamable, .noStreamable)
-                XCTAssertEqual(topTracks.items[0].artist.name, "Artist 0")
-                XCTAssertEqual(topTracks.items[0].artist.mbid, "artist-0-mbid")
-                XCTAssertEqual(topTracks.items[0].artist.url.absoluteString, "https://artist.com/artist-0")
-                XCTAssertEqual(topTracks.items[0].image.small?.absoluteString, "https://images.com/artist-0-s.png")
-                XCTAssertEqual(topTracks.items[0].image.medium?.absoluteString, "https://images.com/artist-0-m.png")
-                XCTAssertEqual(topTracks.items[0].image.large?.absoluteString, "https://images.com/artist-0-l.png")
-                XCTAssertEqual(topTracks.items[0].image.extraLarge?.absoluteString, "https://images.com/artist-0-xl.png")
-                XCTAssertEqual(topTracks.items[0].rank, 1)
-                XCTAssertEqual(topTracks.items[1].name, "Track 1")
-                XCTAssertEqual(topTracks.items[1].duration, 101)
-                XCTAssertEqual(topTracks.items[1].listeners, 910)
-                XCTAssertEqual(topTracks.items[1].mbid, "track-1-mbid")
-                XCTAssertEqual(topTracks.items[1].url.absoluteString, "https://tracks.com/track-1")
-                XCTAssertEqual(topTracks.items[1].streamable, .streamableFullTrack)
-                XCTAssertEqual(topTracks.items[1].artist.name, "Artist 1")
-                XCTAssertEqual(topTracks.items[1].artist.mbid, "artist-1-mbid")
-                XCTAssertEqual(topTracks.items[1].artist.url.absoluteString, "https://artist.com/artist-1")
-                XCTAssertEqual(topTracks.items[1].image.small?.absoluteString, "https://images.com/artist-1-s.png")
-                XCTAssertEqual(topTracks.items[1].image.medium?.absoluteString, "https://images.com/artist-1-m.png")
-                XCTAssertEqual(topTracks.items[1].image.large?.absoluteString, "https://images.com/artist-1-l.png")
-                XCTAssertEqual(topTracks.items[1].image.extraLarge?.absoluteString, "https://images.com/artist-1-xl.png")
-                XCTAssertEqual(topTracks.items[1].rank, 2)
-
-                XCTAssertEqual(topTracks.pagination.total, 500)
-                XCTAssertEqual(topTracks.pagination.totalPages, 100)
-                XCTAssertEqual(topTracks.pagination.perPage, 5)
-                XCTAssertEqual(topTracks.pagination.page, 1)
+                self.validateGeoTopTracks(topTracks)
 
             case .failure(let error):
                 XCTFail("Expected to succeed, but it failed with error \(error)")
@@ -100,7 +175,33 @@ class GeoModuleTests: XCTestCase {
         )
     }
 
-    func test_getTopTracks_failure()  throws {
+    func test_getTopTracks_failure() async throws {
+        let jsonURL = Bundle.module.url(
+            forResource: "Resources/geo.getTopTracksBadParams",
+            withExtension: "json"
+        )!
+
+        let fakeData = try Data(contentsOf: jsonURL)
+        let params = GeoTopTracksParams(country: "bo")
+
+        apiClient.data = fakeData
+        // In real life, for some unknown reason, lastfm
+        // returns a 200 even if country param is wrong.
+        // But here we using a 400 bad request for testing porpuses.
+        apiClient.response = Constants.RESPONSE_400_BAD_REQUEST
+
+        do {
+            let _ = try await instance.getTopTracks(params: params)
+            XCTFail("Expected to fail, but it succedeed")
+        } catch LastFMError.LastFMServiceError(let errorType, let message) {
+            XCTAssertEqual(errorType, .InvalidParameters)
+            XCTAssertEqual(message, "country param required")
+        } catch {
+            XCTFail("Expected to be LastFMServiceErrorType.InvalidParameters")
+        }
+    }
+
+    func test_getTopTracks_failure() throws {
         let jsonURL = Bundle.module.url(
             forResource: "Resources/geo.getTopTracksBadParams",
             withExtension: "json"
@@ -139,6 +240,33 @@ class GeoModuleTests: XCTestCase {
 
     // getTopArtists
 
+    func test_getTopArtists_success() async throws {
+        let jsonURL = Bundle.module.url(
+            forResource: "Resources/geo.getTopArtists",
+            withExtension: "json"
+        )!
+
+        let fakeData = try Data(contentsOf: jsonURL)
+        let params = SearchParams(term: "australia", limit: 5, page: 1)
+
+        apiClient.data = fakeData
+        apiClient.response = Constants.RESPONSE_200_OK
+
+        let topArtist = try await instance.getTopArtists(params: params)
+
+        validateGeoTopArtists(topArtist)
+
+        XCTAssertEqual(apiClient.asyncGetCalls.count, 1)
+        XCTAssertEqual(apiClient.asyncGetCalls[0].headers, nil)
+
+        XCTAssertTrue(
+            Util.areSameURL(
+                apiClient.asyncGetCalls[0].url.absoluteString,
+                "https://ws.audioscrobbler.com/2.0?format=json&api_key=someAPIKey&country=australia&method=geo.gettopartists&limit=5&page=1"
+            )
+        )
+    }
+
     func test_getTopArtists_success() throws {
         let jsonURL = Bundle.module.url(
             forResource: "Resources/geo.getTopArtists",
@@ -155,42 +283,7 @@ class GeoModuleTests: XCTestCase {
         instance.getTopArtists(params: params) { result in
             switch(result) {
             case .success(let topArtists):
-                XCTAssertEqual(topArtists.pagination.page, 1)
-                XCTAssertEqual(topArtists.pagination.perPage, 5)
-                XCTAssertEqual(topArtists.pagination.total, 1297046)
-                XCTAssertEqual(topArtists.pagination.totalPages, 259410)
-
-                XCTAssertEqual(topArtists.items.count, 5)
-                XCTAssertEqual(topArtists.items[0].name, "Radiohead")
-                XCTAssertEqual(topArtists.items[0].listeners, 6214938)
-                XCTAssertEqual(topArtists.items[0].mbid, "a74b1b7f-71a5-4011-9441-d0b5e4122711")
-                XCTAssertEqual(topArtists.items[0].url.absoluteString, "https://www.last.fm/music/Radiohead")
-                XCTAssertEqual(topArtists.items[0].streamable, false)
-
-                XCTAssertEqual(
-                    topArtists.items[0].image.small!.absoluteString,
-                    "https://lastfm.freetls.fastly.net/i/u/34s/2a96cbd8b46e442fc41c2b86b821562f.png"
-                )
-
-                XCTAssertEqual(
-                    topArtists.items[0].image.medium!.absoluteString,
-                    "https://lastfm.freetls.fastly.net/i/u/64s/2a96cbd8b46e442fc41c2b86b821562f.png"
-                )
-
-                XCTAssertEqual(
-                    topArtists.items[0].image.large!.absoluteString,
-                    "https://lastfm.freetls.fastly.net/i/u/174s/2a96cbd8b46e442fc41c2b86b821562f.png"
-                )
-
-                XCTAssertEqual(
-                    topArtists.items[0].image.extraLarge!.absoluteString,
-                    "https://lastfm.freetls.fastly.net/i/u/300x300/2a96cbd8b46e442fc41c2b86b821562f.png"
-                )
-
-                XCTAssertEqual(
-                    topArtists.items[0].image.mega!.absoluteString,
-                    "https://lastfm.freetls.fastly.net/i/u/300x300/2a96cbd8b46e442fc41c2b86b821562f.png"
-                )
+                self.validateGeoTopArtists(topArtists)
             case .failure(let error):
                 XCTFail("It was expected to succeed, but it failed with error \(error.localizedDescription)")
             }
