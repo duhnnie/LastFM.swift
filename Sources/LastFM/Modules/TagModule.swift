03c67ba2618168a833f69cf24cf12d6a3ec56c0d
Daniel Canedo <dnl.canedo@gmail.com>
2024-12-22 18:56:49 -0400

    LF-109 Make TagModule to support async/await

--- Sources/LastFM/Modules/TagModule.swift
+++ Sources/LastFM/Modules/TagModule.swift
@@ -25,6 +25,20 @@ public struct TagModule {
         self.requester = requester
         self.secure = secure
     }
+    
+    @available(iOS 13.0, watchOS 6.0, tvOS 13.0, *)
+    public func getTopTracks(params: SearchParams) async throws -> CollectionPage<TagTopTrack> {
+        let params = parent.normalizeParams(
+            params: params.toDictionary(termKey: "tag"),
+            method: APIMethod.getTopTracks
+        )
+
+        return try await requester.getDataAndParse(
+            params: params,
+            type: CollectionPage<TagTopTrack>.self,
+            secure: self.secure
+        )
+    }
 
     public func getTopTracks(
         params: SearchParams,
@@ -36,6 +50,20 @@ public struct TagModule {
         )
 
         requester.getDataAndParse(params: params, secure: self.secure, onCompletion: onCompletion)
+    }
+    
+    @available(iOS 13.0, watchOS 6.0, tvOS 13.0, *)
+    public func getTopArtists(params: SearchParams) async throws -> CollectionPage<TagTopArtist> {
+        let params = parent.normalizeParams(
+            params: params.toDictionary(termKey: "tag"),
+            method: APIMethod.getTopArtists
+        )
+
+        return try await requester.getDataAndParse(
+            params: params,
+            type: CollectionPage<TagTopArtist>.self,
+            secure: self.secure
+        )
     }
 
     public func getTopArtists(
@@ -50,6 +78,15 @@ public struct TagModule {
         requester.getDataAndParse(params: params, secure: self.secure, onCompletion: onCompletion)
     }
 
+    @available(iOS 13.0, watchOS 6.0, tvOS 13.0, *)
+    public func getTopAlbums(params: SearchParams) async throws -> CollectionPage<TagTopAlbum> {
+        let params = parent.normalizeParams(
+            params: params.toDictionary(termKey: "tag"),
+            method: APIMethod.getTopAlbums
+        )
+
+        return try await requester.getDataAndParse(params: params, type: CollectionPage<TagTopAlbum>.self, secure: self.secure)
+    }
 
     public func getTopAlbums(
         params: SearchParams,
@@ -61,6 +98,23 @@ public struct TagModule {
         )
 
         requester.getDataAndParse(params: params, secure: self.secure, onCompletion: onCompletion)
+    }
+    
+    @available(iOS 13.0, watchOS 6.0, tvOS 13.0, *)
+    public func getInfo(name: String, lang: String? = nil) async throws -> TagInfo {
+        var params = ["name": name]
+
+        if let lang = lang {
+            params["lang"] = lang
+        }
+
+        let normalizedParams = parent.normalizeParams(params: params, method: APIMethod.getInfo)
+        
+        return try await requester.getDataAndParse(
+            params: normalizedParams,
+            type: TagInfo.self,
+            secure: self.secure
+        )
     }
 
     public func getInfo(
@@ -83,6 +137,20 @@ public struct TagModule {
         )
     }
 
+    @available(iOS 13.0, watchOS 6.0, tvOS 13.0, *)
+    public func getWeeklyChartList(tag: String) async throws -> CollectionList<ChartDateRange> {
+        let params = parent.normalizeParams(
+            params: ["tag": tag],
+            method: APIMethod.getWeeklyChartList
+        )
+
+        return try await requester.getDataAndParse(
+            params: params,
+            type: CollectionList<ChartDateRange>.self,
+            secure: self.secure
+        )
+    }
+    
     public func getWeeklyChartList(
         tag: String,
         onCompletion: @escaping LastFM.OnCompletion<CollectionList<ChartDateRange>>
@@ -93,6 +161,20 @@ public struct TagModule {
         )
 
         requester.getDataAndParse(params: params, secure: self.secure, onCompletion: onCompletion)
+    }
+    
+    @available(iOS 13.0, watchOS 6.0, tvOS 13.0, *)
+    public func getSimilar(tag: String) async throws -> CollectionList<SimilarTag> {
+        let params = parent.normalizeParams(
+            params: ["tag": tag],
+            method: APIMethod.getSimilar
+        )
+
+        return try await requester.getDataAndParse(
+            params: params,
+            type: CollectionList<SimilarTag>.self,
+            secure: self.secure
+        )
     }
 
     public func getSimilar(
@@ -105,6 +187,26 @@ public struct TagModule {
         )
 
         requester.getDataAndParse(params: params, secure: self.secure, onCompletion: onCompletion)
+    }
+    
+    @available(iOS 13.0, watchOS 6.0, tvOS 13.0, *)
+    public func getTopTags(
+        offset: UInt = 0,
+        limit: UInt = 10
+    ) async throws -> CollectionList<TopGlobalTag> {
+        let params = parent.normalizeParams(
+            params: [
+                "offset": String(offset),
+                "num_res": String(limit)
+            ],
+            method: APIMethod.getTopTags
+        )
+
+        return try await requester.getDataAndParse(
+            params: params,
+            type: CollectionList<TopGlobalTag>.self,
+            secure: self.secure
+        )
     }
 
     public func getTopTags(
