435d4cd81d67b5bb52aafebc06199ab10e18f99f
Daniel Canedo <dnl.canedo@gmail.com>
2024-12-22 21:34:29 -0400

    LF-109 Make UserModule support async/await

--- Sources/LastFM/Modules/UserModule.swift
+++ Sources/LastFM/Modules/UserModule.swift
@@ -30,6 +30,18 @@ public struct UserModule {
         self.requester = requester
         self.secure = secure
     }
+    
+    @available(iOS 13.0, watchOS 6.0, tvOS 13.0, *)
+    private func getBaseRecentTracks<T: Decodable>(
+        params: RecentTracksParams,
+        extended: Bool
+    ) async throws -> CollectionPage<T> {
+        var params = parent.normalizeParams(params: params, method: APIMethod.getRecentTracks)
+
+        params["extended"] = extended ? "1" : "0"
+
+        return try await requester.getDataAndParse(params: params, type: CollectionPage<T>.self, secure: self.secure)
+    }
 
     private func getBaseRecentTracks<T: Decodable>(
         params: RecentTracksParams,
@@ -42,6 +54,13 @@ public struct UserModule {
 
         requester.getDataAndParse(params: params, secure: self.secure, onCompletion: onCompletion)
     }
+    
+    @available(iOS 13.0, watchOS 6.0, tvOS 13.0, *)
+    public func getRecentTracks(
+        params: RecentTracksParams
+    ) async throws -> CollectionPage<RecentTrack> {
+        return try await getBaseRecentTracks(params: params, extended: false)
+    }
 
     public func getRecentTracks(
         params: RecentTracksParams,
@@ -49,12 +68,28 @@ public struct UserModule {
     ) {
         getBaseRecentTracks(params: params, extended: false, onCompletion: onCompletion)
     }
+    
+    @available(iOS 13.0, watchOS 6.0, tvOS 13.0, *)
+    public func getExtendedRecentTracks(
+        params: RecentTracksParams
+    ) async throws -> CollectionPage<ExtendedRecentTrack> {
+        return try await getBaseRecentTracks(params: params, extended: true)
+    }
 
     public func getExtendedRecentTracks(
         params: RecentTracksParams,
         onCompletion: @escaping LastFM.OnCompletion<CollectionPage<ExtendedRecentTrack>>
     ) {
         getBaseRecentTracks(params: params, extended: true, onCompletion: onCompletion)
+    }
+    
+    @available(iOS 13.0, watchOS 6.0, tvOS 13.0, *)
+    public func getTopTracks(
+        params: UserTopItemsParams
+    ) async throws -> CollectionPage<UserTopTrack> {
+        let params = parent.normalizeParams(params: params, method: APIMethod.getTopTracks)
+
+        return try await requester.getDataAndParse(params: params, type: CollectionPage<UserTopTrack>.self, secure: self.secure)
     }
 
     public func getTopTracks(
@@ -64,6 +99,22 @@ public struct UserModule {
         let params = parent.normalizeParams(params: params, method: APIMethod.getTopTracks)
 
         requester.getDataAndParse(params: params, secure: self.secure, onCompletion: onCompletion)
+    }
+    
+    @available(iOS 13.0, watchOS 6.0, tvOS 13.0, *)
+    public func getWeeklyTrackChart(
+        params: UserWeeklyChartParams
+    ) async throws -> CollectionList<UserWeeklyTrackChart> {
+        let params = parent.normalizeParams(
+            params: params,
+            method: APIMethod.getWeeklyTrackChart
+        )
+
+        return try await requester.getDataAndParse(
+            params: params,
+            type: CollectionList<UserWeeklyTrackChart>.self,
+            secure: self.secure
+        )
     }
 
     public func getWeeklyTrackChart(
@@ -77,6 +128,20 @@ public struct UserModule {
 
         requester.getDataAndParse(params: params, secure: true, onCompletion: onCompletion)
     }
+    
+    @available(iOS 13.0, watchOS 6.0, tvOS 13.0, *)
+    public func getLovedTracks(params: SearchParams) async throws -> CollectionPage<LovedTrack> {
+        let params = parent.normalizeParams(
+            params: params.toDictionary(termKey: "user"),
+            method: APIMethod.getLovedTracks
+        )
+
+        return try await requester.getDataAndParse(
+            params: params,
+            type: CollectionPage<LovedTrack>.self,
+            secure: self.secure
+        )
+    }
 
     public func getLovedTracks(
         params: SearchParams,
@@ -89,6 +154,19 @@ public struct UserModule {
 
         requester.getDataAndParse(params: params, secure: true, onCompletion: onCompletion)
     }
+    
+    @available(iOS 13.0, watchOS 6.0, tvOS 13.0, *)
+    public func getTopArtists(
+        params: UserTopItemsParams
+    ) async throws -> CollectionPage<UserTopArtist> {
+        let params = parent.normalizeParams(params: params, method: APIMethod.getTopArtists)
+
+        return try await requester.getDataAndParse(
+            params: params,
+            type: CollectionPage<UserTopArtist>.self,
+            secure: self.secure
+        )
+    }
 
     public func getTopArtists(
         params: UserTopItemsParams,
@@ -97,6 +175,19 @@ public struct UserModule {
         let params = parent.normalizeParams(params: params, method: APIMethod.getTopArtists)
 
         requester.getDataAndParse(params: params, secure: self.secure, onCompletion: onCompletion)
+    }
+    
+    @available(iOS 13.0, watchOS 6.0, tvOS 13.0, *)
+    public func getWeeklyArtistChart(
+        params: UserWeeklyChartParams
+    ) async throws -> CollectionList<UserWeeklyArtistChart> {
+        let params = parent.normalizeParams(params: params, method: APIMethod.getWeeklyArtistChart)
+
+        return try await requester.getDataAndParse(
+            params: params,
+            type: CollectionList<UserWeeklyArtistChart>.self,
+            secure: self.secure
+        )
     }
 
     public func getWeeklyArtistChart(
@@ -107,6 +198,19 @@ public struct UserModule {
 
         requester.getDataAndParse(params: params, secure: true, onCompletion: onCompletion)
     }
+    
+    @available(iOS 13.0, watchOS 6.0, tvOS 13.0, *)
+    public func getTopAlbums(
+        params: UserTopItemsParams
+    ) async throws -> CollectionPage<UserTopAlbum> {
+        let params = parent.normalizeParams(params: params, method: APIMethod.getTopAlbums)
+
+        return try await requester.getDataAndParse(
+            params: params,
+            type: CollectionPage<UserTopAlbum>.self,
+            secure: self.secure
+        )
+    }
 
     public func getTopAlbums(
         params: UserTopItemsParams,
@@ -115,6 +219,19 @@ public struct UserModule {
         let params = parent.normalizeParams(params: params, method: APIMethod.getTopAlbums)
 
         requester.getDataAndParse(params: params, secure: self.secure, onCompletion: onCompletion)
+    }
+    
+    @available(iOS 13.0, watchOS 6.0, tvOS 13.0, *)
+    public func getWeeklyAlbumChart(
+        params: UserWeeklyChartParams
+    ) async throws -> CollectionList<UserWeeklyAlbumChart> {
+        let params = parent.normalizeParams(params: params, method: APIMethod.getWeeklyAlbumChart)
+
+        return try await requester.getDataAndParse(
+            params: params,
+            type: CollectionList<UserWeeklyAlbumChart>.self,
+            secure: self.secure
+        )
     }
 
     public func getWeeklyAlbumChart(
@@ -125,6 +242,17 @@ public struct UserModule {
 
         requester.getDataAndParse(params: params, secure: true, onCompletion: onCompletion)
     }
+    
+    @available(iOS 13.0, watchOS 6.0, tvOS 13.0, *)
+    public func getInfo(user: String) async throws -> UserInfo {
+        let params = parent.normalizeParams(params: ["user": user], method: APIMethod.getInfo)
+
+        return try await requester.getDataAndParse(
+            params: params,
+            type: UserInfo.self,
+            secure: self.secure
+        )
+    }
 
     public func getInfo(user: String, onCompletion: @escaping LastFM.OnCompletion<UserInfo>) {
         let params = parent.normalizeParams(params: ["user": user], method: APIMethod.getInfo)
@@ -132,10 +260,35 @@ public struct UserModule {
         requester.getDataAndParse(params: params, secure: true, onCompletion: onCompletion)
     }
     
+    @available(iOS 13.0, watchOS 6.0, tvOS 13.0, *)
+    public func getInfo(sessionKey: String) async throws -> UserInfo {
+        let params = parent.normalizeParams(params: [:], method: APIMethod.getInfo, sessionKey: sessionKey)
+
+        return try await requester.postFormURLEncodedAndParse(
+            payload: params,
+            type: UserInfo.self,
+            secure: self.secure
+        )
+    }
+    
     public func getInfo(sessionKey: String, onCompletion: @escaping LastFM.OnCompletion<UserInfo>) throws {
         let params = parent.normalizeParams(params: [:], method: APIMethod.getInfo, sessionKey: sessionKey)
 
         try requester.postFormURLEncodedAndParse(payload: params, secure: true, onCompletion: onCompletion)
+    }
+    
+    @available(iOS 13.0, watchOS 6.0, tvOS 13.0, *)
+    public func getFriends(params: SearchParams) async throws -> CollectionPage<UserPublicInfo> {
+        let params = parent.normalizeParams(
+            params: params.toDictionary(termKey: "user"),
+            method: APIMethod.getFriends
+        )
+
+        return try await requester.getDataAndParse(
+            params: params,
+            type: CollectionPage<UserPublicInfo>.self,
+            secure: self.secure
+        )
     }
 
     public func getFriends(
@@ -148,6 +301,20 @@ public struct UserModule {
         )
 
         requester.getDataAndParse(params: params, secure: true, onCompletion: onCompletion)
+    }
+    
+    @available(iOS 13.0, watchOS 6.0, tvOS 13.0, *)
+    public func getWeeklyChartList(user: String) async throws -> CollectionList<ChartDateRange> {
+        let params = parent.normalizeParams(
+            params: ["user": user],
+            method: APIMethod.getWeeklyChartList
+        )
+
+        return try await requester.getDataAndParse(
+            params: params,
+            type: CollectionList<ChartDateRange>.self,
+            secure: self.secure
+        )
     }
 
     public func getWeeklyChartList(
