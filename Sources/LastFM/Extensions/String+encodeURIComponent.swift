781bdd60959c3eddee15e82c23ab208cfa179e6e
Daniel Canedo <dnl.canedo@gmail.com>
2025-03-19 15:52:30 -0400

    LF-116 Fix incorrect encoding for query params

---
+++ Sources/LastFM/Extensions/String+encodeURIComponent.swift
@@ -0,0 +1,18 @@
+//
+//  File.swift
+//  
+//
+//  Created by Daniel on 19/3/25.
+//
+
+import Foundation
+
+extension String {
+    
+    private static let uriComponentCharacterSet = CharacterSet.urlPathAllowed.subtracting(CharacterSet(["/", "+"]))
+
+    func encodeURIComponent() -> String? {
+        return self.addingPercentEncoding(withAllowedCharacters: Self.uriComponentCharacterSet)
+    }
+
+}
---
+++ Sources/LastFM/Extensions/URLComponents+lastFMUrl.swift
@@ -0,0 +1,26 @@
+//
+//  File.swift
+//  
+//
+//  Created by Daniel on 19/3/25.
+//
+
+import Foundation
+
+extension URLComponents {
+    
+    // Returns a custom lastfm-encoded url
+    var lastFMUrl: URL? {
+        var urlComponents = self
+        urlComponents.percentEncodedQuery = self.queryItems?.map({ item in
+            guard let name = item.name.encodeURIComponent() else {
+                return ""
+            }
+            
+            return "\(name)=\(item.value?.encodeURIComponent() ?? "")"
+        }).joined(separator: "&")
+    
+        return urlComponents.url
+    }
+    
+}
